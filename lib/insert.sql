80afd2932b407720b04c0d95c2ef08a318b0606f
louis-ngugi <louisngugi858@gmail.com>
2025-03-03 02:47:42 +0300

    update insert.sql create.sql and sql_queries.sql

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear1', 5, 'M', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear2', 7, 'F', 'Black', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear3', 3, 'M', 'Golden', 'Playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear4', 10, 'F', 'White', 'Gentle', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear5', 6, 'M', 'Gray', 'Curious', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear6', 4, 'F', 'Brown', 'Shy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Bear7', 8, 'M', 'Black', 'Aggressive', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 2, 'F', 'Golden', 'Friendly', 1);
